8d1fa017bc561a640b8ba59933bc8322be2f0836
tmidhat <tarikmidhat@gmail.com>
2014-12-10 20:12:07 +0100

    XXXtreme Programming
    Denis i Nera dodavanje soba

--- Hotel2/Controllers/HomeController.cs
+++ Hotel2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Hotel2.Models;
 
 namespace Hotel2.Controllers
 {
@@ -13,6 +14,10 @@ namespace Hotel2.Controllers
 
         public ActionResult Index()
         {
+            DBCResortEntities db = new DBCResortEntities();
+            ViewBag.idEmpoyee = new SelectList(db.Employees, "idEmplyee", "Username");
+            ViewBag.idGuest = new SelectList(db.Guests, "idGuest", "Username");
+            ViewBag.idRoom = new SelectList(db.Rooms, "idRoom", "Status");
             return View();
         }
 
